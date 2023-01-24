c9c235e72db091c4a1e76b3522eddedf574c76ae
wskoper <w_koper@ms.wwsi.edu.pl>
2023-01-24 16:00:53 +0100

    Initial commit

---
+++ w_koper_DBAuta/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace w_koper_DBAuta.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
